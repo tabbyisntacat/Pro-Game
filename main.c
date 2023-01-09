d24e08110b8999b89a75201c52f36043f56dba86
tabbyisntacat <107060859+tabbyisntacat@users.noreply.github.com>
2023-01-08 20:06:00 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+// If you are trying to learn from my code.... PLEASE LOOK ELSE WHERE THIS IS REALLY BAD CODE
 
 int main(){
     int cash;
@@ -20,11 +20,13 @@ int main(){
     }
 
     if (winner == 1) {
+        // useless if statement i just hate my life
         printf("Since yu have won, You now owe me head\n\n\n\n");
         printf("This game was really shitly made for memes, the games source code is uploaded to my github\n");
         printf("Github: https://github.com/tabbyisntacat?tab=repositories");
         printf("Press enter to close...");
     }
+    // Idk any other ways to make sure the console doesnt say goodbye every time i run it
     getchar(); 
     getchar();
     return 0; 
